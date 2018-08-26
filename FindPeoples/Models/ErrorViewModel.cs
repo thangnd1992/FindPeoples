f37497b43c1cf280b1abba2bc7f4899b6d7e0750
thangnd <38719418+thangnd1992@users.noreply.github.com>
2018-08-26 11:07:22 +0700

    Add project files.

---
+++ FindPeoples/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FindPeoples.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
